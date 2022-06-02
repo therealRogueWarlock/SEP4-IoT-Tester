87c8e27005e4adc3f69010a0615782595fe1b127
therealRogueWarlock <58672117+therealRogueWarlock@users.noreply.github.com>
2022-06-02 12:42:58 +0200

    Tried VARARG FFF
    Dident work

--- SEP4-IoT-TesterProject/FFF_Mocks/FFF_mocksDeclarations.h
+++ SEP4-IoT-TesterProject/FFF_Mocks/FFF_mocksDeclarations.h
@@ -4,10 +4,16 @@
 extern "C" {
 #include "hih8120.h"
 #include "portable.h"
+//#include "thread_safe_printf.h"
 }
-
+/*
 DECLARE_FAKE_VALUE_FUNC(void*, pvPortMalloc, size_t);
 DECLARE_FAKE_VOID_FUNC(vPortFree, void*)
+*/
+
+DECLARE_FAKE_VOID_FUNC(thread_safe_printf_initialize);
+
+DECLARE_FAKE_VOID_FUNC_VARARG(thread_safe_printf, const char* fmt, ...);
 
 DECLARE_FAKE_VALUE_FUNC(hih8120_driverReturnCode_t, hih8120_initialise);
 DECLARE_FAKE_VALUE_FUNC(hih8120_driverReturnCode_t, hih8120_wakeup);
--- SEP4-IoT-TesterProject/FFF_Mocks/FFF_mocksDefinitions.cpp
+++ SEP4-IoT-TesterProject/FFF_Mocks/FFF_mocksDefinitions.cpp
@@ -1,9 +1,15 @@
 #pragma once
 #include "FFF_mocksDeclarations.h"
 
-
+/*
 DEFINE_FAKE_VALUE_FUNC(void*, pvPortMalloc, size_t);
 DEFINE_FAKE_VOID_FUNC(vPortFree, void*)
+*/
+
+DEFINE_FAKE_VOID_FUNC(thread_safe_printf_initialize);
+
+DEFINE_FAKE_VOID_FUNC_VARARG(thread_safe_printf, const char *fmt, ...);
+
 
 DEFINE_FAKE_VALUE_FUNC(hih8120_driverReturnCode_t, hih8120_initialise);
 DEFINE_FAKE_VALUE_FUNC(hih8120_driverReturnCode_t, hih8120_wakeup);
